f059de062f2f73a5f34292f88a173f7fc5c36d96
cedric <cedric.centeno@gmail.com>
2023-09-08 00:04:39 +0200

    set configuration to online for release

--- app/src/main/java/ch/heigvd/pdg_grocerypal/config/Configuration.kt
+++ app/src/main/java/ch/heigvd/pdg_grocerypal/config/Configuration.kt
@@ -1,5 +1,8 @@
 package ch.heigvd.pdg_grocerypal.config
 
 object Configuration {
-    const val BaseURL = "http://10.0.2.2:8080"
+
+    // Local back-end URL = "http://10.0.2.2:8080"
+    // Online back-end URL = "http://35.246.127.184:8080/"
+    const val BaseURL = "http://35.246.127.184:8080/"
 }
