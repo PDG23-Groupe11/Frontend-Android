57e0d5115e24675ffd15a0a7d2aa5cb8d427d214
cedric <cedric.centeno@gmail.com>
2023-09-08 11:30:17 +0200

    fixed online url

--- app/src/main/java/ch/heigvd/pdg_grocerypal/config/Configuration.kt
+++ app/src/main/java/ch/heigvd/pdg_grocerypal/config/Configuration.kt
@@ -3,6 +3,6 @@ package ch.heigvd.pdg_grocerypal.config
 object Configuration {
 
     // Local back-end URL = "http://10.0.2.2:8080"
-    // Online back-end URL = "http://35.246.127.184:8080/"
-    const val BaseURL = "http://35.246.127.184:8080/"
+    // Online back-end URL = "http://35.246.127.184:8080"
+    const val BaseURL = "http://35.246.127.184:8080"
 }
